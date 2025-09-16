ecfb81366074890d6e59608b6ae66872bfd1c3f7
Massimiliano Melillo <m.melillo148@gmail.com>
2025-09-16 15:09:08 +0200

    Init

---
+++ docs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Tractor(models.Model):
+    plate = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return self.plate
+    
+class Trailer(models.Model):
+    plate = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return self.plate
+    
